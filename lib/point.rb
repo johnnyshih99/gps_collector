c3a11c234eabe62d67cd2f641674e34197af48f4
Johnny Shih W <johnnyshih99@gmailcom>
2021-10-11 20:23:25 -0400

    refactor: separate out insert_points and get_points for requests

--- lib/point.rb
+++ lib/point.rb
@@ -14,26 +14,7 @@ class Point
       params = JSON.parse(@request.body.read)
       case @request.path
       when "/"
-        geometries = nil
-        if params.is_a?(Array)
-          geometries = params
-        elsif params.is_a?(Hash) && params["type"] == "GeometryCollection"
-          geometries = params["geometries"]
-        else
-          return bad_request
-        end
-
-        values = []
-        geometries.each do |geom|
-          if geom["type"] == "Point"
-            coord = geom["coordinates"]
-            values << "('POINT(#{coord.join(" ")})')"
-          end
-        end
-        unless values.empty?
-          query = "INSERT INTO points VALUES #{values.join(",")}"
-          @db.exec(query)
-        end
+        response = insert_points(params)
       else
         return not_found
       end
@@ -41,51 +22,76 @@ class Point
       params = @request.params
       case @request.path
       when "/"
-        if params["type"] == "Point"
-          coord = params['coordinates']
-          radius = params["radius"].to_i
-
-          query = <<-SQL
-            SELECT ST_AsGeoJSON(pt) FROM points
-            WHERE ST_DWithin(
-              points.pt,
-              ST_Point(#{coord}),
-              #{radius}
-            );
-          SQL
-          result = []
-          @db.exec(query) do |res|
-            res.each do |row|
-              result << JSON.parse(row["st_asgeojson"])
-            end
-          end
-          response.write(result.to_json)
-        elsif params["type"] == "Polygon"
-          lineString = params["lineString"]
-          
-          query = <<-SQL
-            SELECT ST_AsGeoJSON(pt) FROM points
-            WHERE ST_Within(
-              points.pt,
-              ST_MakePolygon('LINESTRING(#{lineString})')
-            );
-          SQL
-          result = []
-          @db.exec(query) do |res|
-            res.each do |row|
-              result << JSON.parse(row["st_asgeojson"])
-            end
-          end
-          response.write(result.to_json)
-        else
-          return bad_request
-        end
+        response = get_points(params)
       else
         return not_found
       end
     else
       return not_found
     end
+    response
+  end
+
+  def insert_points(params)
+    response = Rack::Response.new
+    geometries = nil
+    if params.is_a?(Array)
+      geometries = params
+    elsif params.is_a?(Hash) && params["type"] == "GeometryCollection"
+      geometries = params["geometries"]
+    else
+      return bad_request
+    end
+
+    values = []
+    geometries.each do |geom|
+      if geom["type"] == "Point"
+        coord = geom["coordinates"]
+        values << "('POINT(#{coord.join(" ")})')"
+      end
+    end
+    unless values.empty?
+      query = "INSERT INTO points VALUES #{values.join(",")}"
+      @db.exec(query)
+    end
+    response
+  end
+
+  def get_points(params)
+    response = Rack::Response.new
+    if params["type"] == "Point"
+      coord = params['coordinates']
+      radius = params["radius"].to_i
+
+      query = <<-SQL
+        SELECT ST_AsGeoJSON(pt) FROM points
+        WHERE ST_DWithin(
+          points.pt,
+          ST_Point(#{coord}),
+          #{radius}
+        );
+      SQL
+    elsif params["type"] == "Polygon"
+      lineString = params["lineString"]
+      
+      query = <<-SQL
+        SELECT ST_AsGeoJSON(pt) FROM points
+        WHERE ST_Within(
+          points.pt,
+          ST_MakePolygon('LINESTRING(#{lineString})')
+        );
+      SQL
+    else
+      return bad_request
+    end
+
+    result = []
+    @db.exec(query) do |res|
+      res.each do |row|
+        result << JSON.parse(row["st_asgeojson"])
+      end
+    end
+    response.write(result.to_json)
     response
   end
 
